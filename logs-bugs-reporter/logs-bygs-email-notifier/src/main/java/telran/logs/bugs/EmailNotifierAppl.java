87d759dd7a347955edcee18a58bda32cd34180ce
David Itzhak <daviditzhak7@gmail.com>
2021-02-09 22:29:17 +0200

    hw62

--- logs-bugs-reporter/logs-bygs-email-notifier/src/main/java/telran/logs/bugs/EmailNotifierAppl.java
+++ logs-bugs-reporter/logs-bygs-email-notifier/src/main/java/telran/logs/bugs/EmailNotifierAppl.java
@@ -1,23 +1,10 @@
 package telran.logs.bugs;
 
-import java.util.function.Consumer;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-
-import lombok.extern.slf4j.Slf4j;
-import telran.logs.bugs.client.EmailProviderClient;
-import telran.logs.bugs.dto.LogDto;
 
 @SpringBootApplication
-@Slf4j
 public class EmailNotifierAppl {
-
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailNotifierAppl.class, args);
